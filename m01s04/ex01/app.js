43e33eb298faa3161f934867d074f6e9e5fa748c
mirela2205 <mirela.dina88@gmail.com>
2022-12-02 07:34:06 +0200

    clasas04

--- m01s04/ex01/app.js
+++ m01s04/ex01/app.js
@@ -1,14 +1,30 @@
-var number = prompt('Introdu un numar');
+var userInput = prompt('Introdu un numar');
+var referenceNumber = 20;
 var message = '';
 var messageElement = document.getElementById('message');
+var number = Number(userInput);
+// number poate fi un numar sau NaN
+// isNaN(number) || userInput === null
 
-if (number % 2 === 0) {
-  // console.log('Numarul este par');
-  message - 'Numarul este par';
+if (userInput === null || isNaN(number) || userInput.trim().length === 0) {
+  message = 'Nu ai introdus un numar';
 } else {
-  // console.log('Numarul este impar');
-  message - 'Numarul este impar';
+  if (number > referenceNumber) {
+    message = `Numarul este mai mare decat ${referenceNumber}`;
+  } else if (number === referenceNumber) {
+    message = `Numarul este egal cu ${referenceNumber}`;
+  } else {
+    message = `Numarul este mai mic decat ${referenceNumber}`;
+  }
 }
 
 console.log(message);
 messageElement.innerText = message;
+
+// if (number % 2 === 0) {
+//   // console.log('Numarul este par');
+//   message = 'Numarul este par';
+// } else {
+//   // console.log('Numarul este impar');
+//   message = 'Numarul este impar';
+// }
---
+++ m01s04/ex04/app.js
@@ -0,0 +1,41 @@
+// for (var i = 1; i < 100; i++) {
+//   console.log(i);
+// }
+
+// 1st
+
+// for (var i = 1; i < 100; i++) {
+//   console.log(i);
+
+//   if (i === 50) {
+//     break;
+//   }
+// }
+
+// 2nd
+
+// for (var i = 1; i <= 100; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+// 3rd
+
+// for (var i = 1; i <= 100; i++) {
+//   if (i % 2 !== 0) {
+//     continue;
+//   }
+
+//   console.log(i);
+// }
+
+// 4th
+
+// for (var i = 1; i <= 100; i++) {
+//   if (i % 5 !== 0) {
+//     continue;
+//   }
+
+//   console.log(i);
+// }
