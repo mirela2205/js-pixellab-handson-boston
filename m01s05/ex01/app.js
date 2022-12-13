eb785531fb4ebc1a9a61a137b05693bcd025d75e
mirela2205 <mirela.dina88@gmail.com>
2022-12-13 22:00:16 +0200

    s04ex13 & s05ex01-04 clasa

---
+++ m01s05/ex01/app.js
@@ -0,0 +1,21 @@
+function getTime() {
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  const milliseconds = date.getMilliseconds();
+
+  const exactTime = `${hours}:${minutes}:${seconds}:${milliseconds}`;
+
+  return exactTime;
+}
+
+console.log(`${getTime()}, am ajuns la Pixellab`);
+
+// console.log(
+//   `
+// ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}:${new Date().getMilliseconds()}
+// `,
+//   'Am ajuns la Pixellab',
+// );
+// cod scris prima data
