d8492905a28643cf534f132db494a64d18d7eebe
mirela2205 <mirela.dina88@gmail.com>
2023-02-14 20:06:57 +0200

    m02s01 ex04 not final

--- m02s01/ex04/app.js
+++ m02s01/ex04/app.js
@@ -21,14 +21,13 @@ class Car {
     this.frame.append(this.car);
 
     // roof?
-    this.carTop = document.createElement('div');
-    this.carTop.classList.add('car__top');
-    this.carTop.style.backgroundColor = this.color;
+    this.carTop = this.createElement('div', ['car__top']);
+    // this.carTop.style.backgroundColor = this.color;
+    this.carTop.style.backgroundColor = 'blue';
     this.car.append(this.carTop);
 
     // body
-    this.carBody = document.createElement('div');
-    this.carBody.classList.add('car__body');
+    this.carBody = this.createElement('div', ['car__body']);
     this.carBody.style.backgroundColor = this.color;
     this.car.append(this.carBody);
 
@@ -37,24 +36,27 @@ class Car {
     this.carBody.append(this.lightBack);
 
     // light front
-    this.lightFront = document.createElement('div');
-    this.lightFront.classList.add('light', 'light--front');
+    this.lightFront = this.createElement('div', ['light', 'light--front']);
     this.carBody.append(this.lightFront);
 
     // wheel back
-    this.wheelBack = document.createElement('div');
-    this.wheelBack.classList.add('wheel', 'car__wheel', 'car__wheel--back');
+    this.wheelBack = this.createElement('div', [
+      'wheel',
+      'car__wheel',
+      'car__wheel--back',
+    ]);
     this.carBody.append(this.wheelBack);
-    this.wheelCapBack = document.createElement('div');
-    this.wheelCapBack.classList.add('wheel__cap');
+    this.wheelCapBack = this.createElement('div', ['wheel__cap']);
     this.wheelBack.append(this.wheelCapBack);
 
     // wheel front
-    this.wheelFront = document.createElement('div');
-    this.wheelFront.classList.add('wheel', 'car__wheel', 'car__wheel--front');
+    this.wheelFront = this.createElement('div', [
+      'wheel',
+      'car__wheel',
+      'car__wheel--front',
+    ]);
     this.carBody.append(this.wheelFront);
-    this.wheelCapFront = document.createElement('div');
-    this.wheelCapFront.classList.add('wheel__cap');
+    this.wheelCapFront = this.createElement('div', ['wheel__cap']);
     this.wheelFront.append(this.wheelCapFront);
   }
 
@@ -62,16 +64,24 @@ class Car {
     // this.lightFront.style.backgroundColor = 'yellow';
     this.lightFront.classList.add('light--on');
     this.areLightsOn = true;
+
+    return this;
   }
 
   turnLightsOff() {
     this.lightFront.classList.remove('light--on');
     this.areLightsOn = false;
+
+    return this;
   }
 
   changePosition(left = 0, top = 0) {
     this.frame.style.left = `${left}px`;
     this.frame.style.top = `${top}px`;
+
+    this.frame.style.transition = 'all 2s';
+
+    return this;
   }
 
   createElement(nodeName = '', classListArray = []) {
